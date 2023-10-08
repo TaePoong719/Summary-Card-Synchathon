ba7d1dc0d06db46756981c56788a178ea571911c
TaePoong719 <just5538@gmail.com>
2023-10-08 15:49:09 +0900

    Feat: Search bar 기본 형태

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Search = () => {
   const handleInput = () => {
@@ -6,13 +7,35 @@ const Search = () => {
   }
   return (
     <span>
-      <input type="text" />
+      <InputSearch type="text" />
       <span>
         <span>|</span>
-        <img alt="돋보기" />
+        <SearchImg src={`${import.meta.env.BASE_URL}search_icon.svg`} alt="돋보기"></SearchImg>
       </span>
     </span>
   )
 }
 
+const InputSearch = styled.input`
+  height: 30px;
+`
+
+const SearchImg = styled.img`
+  display: inline;
+  width: 30px;
+  height: 30px;
+  position: relative;
+  background-color: black;
+  border-radius: 50%;
+  padding: 10px;
+
+  &:after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.5);
+  }
+`
 export default Search
