d76e162a5ba8a70e1d01918ea96abb3865118f8c
TaePoong719 <just5538@gmail.com>
2023-10-10 13:44:14 +0900

    Feat: 커스텀 훅 추가(디바운스, 바깥 클릭 시 모달 제거

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,19 @@
+/* useDebounce Custom Hooks */
+
+import { useEffect, useState } from 'react'
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value)
+    }, delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+
+  return debounceValue
+}
