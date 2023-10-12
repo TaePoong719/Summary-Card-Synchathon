60063455abffe9d4669e2d542b6b25120685f919
TaePoong719 <just5538@gmail.com>
2023-10-12 16:39:31 +0900

    Fix: 사이드바 반응형 수정

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -80,8 +80,8 @@ const MobileContainer = styled.aside`
     position: absolute;
     top: 0;
     left: 0;
-    width: 100vw;
-    height: 100vh;
+    width: 100%;
+    height: 100%;
     background: white;
   }
   .sidebar__openSidebar-icon {
